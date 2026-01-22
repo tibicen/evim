d77fb4d03c301b49beb517d669e1262be6758086
Dawid Huczyński <dawid.huczynski@gmail.com>
2026-01-22 23:32:09 +0100

    fix logo, editorial on services

---
+++ _app/immutable/nodes/6.YIm16MiL.js
@@ -0,0 +1,34 @@
+import{f as e,a}from"../chunks/ktMa7H4X.js";import"../chunks/XnH3fmMk.js";import{e as z,$ as w}from"../chunks/RVnVB21u.js";import{h as t}from"../chunks/BKN0VoXH.js";var c=e('<meta name="description" content="Informacje dotyczące ochrony danych osobowych w spółce Evim."/>'),s=e(`<section class="relative overflow-hidden pt-32 pb-20 lg:pt-48 lg:pb-32"><div class="mb-12 text-center"><span class="text-sm font-semibold tracking-wider text-evim-green uppercase">Aktualności</span> <h2 class="mt-2 text-3xl font-bold text-pretty text-evim-dark">KLAUZULA INFORMACYJNA (RODO)</h2></div> <div class="m-auto mb-12 max-w-3xl p-5 text-justify text-balance text-gray-600"><p class="mt-4">Administratorem danych osobowych pozyskanych i przetwarzanych w związku z zawarciem i
+			realizacją Umowy jest EVIM sp. z o.o. z siedzibą i adresem przy ul. Malachitowa 7, 52-215
+			Wrocław. Dane osobowe przetwarzane są w celu zawarcia i realizacji postanowień Umowy (podstawa
+			prawna przetwarzania: art. 6 ust. 1 lit. b) Rozporządzenia Parlamentu Europejskiego i Rady
+			(UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z
+			przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia
+			dyrektywy 95/46/WE („RODO”) - przetwarzanie jest niezbędne do wykonania umowy, której stroną
+			jest osoba, której dane dotyczą, lub do podjęcia działań na żądanie osoby, której dane
+			dotyczą, przed zawarciem umowy). W tym celu dane będą przechowywane przez okres niezbędny do
+			zawarcia Umowy i okres jej wykonywania.</p> <p class="mt-4">Dane osobowe przetwarzane są również w celach archiwalnych (dowodowych) na wypadek prawnej
+			potrzeby wykazania faktów oraz w celu ewentualnego ustalenia, dochodzenia lub obrony przed
+			roszczeniami (podstawa prawna przetwarzania: art. 6 ust. 1 lit. f) RODO - przetwarzanie jest
+			niezbędne do celów wynikających z prawnie uzasadnionych interesów realizowanych przez
+			administratora). Informuje się, iż prawnie uzasadnionym interesem realizowanym przez
+			administratora jest ustalenie, dochodzenie, obrona roszczeń, prawna potrzeba wykazania
+			określonych okoliczności. W tym celu dane będą przechowywane przez okres niezbędny do
+			realizacji tego celu, jednak nie dłużej niż do momentu uwzględnienia wniesionego przez
+			Panią/Pana sprzeciwu lub nie dłużej niż okres przedawnienia roszczeń.</p> <p class="mt-4">Dane osobowe przetwarzane są nadto w przypadkach, gdy wymaga tego wykonanie obowiązków
+			prawnych ciążących na administratorze, w tym przepisów o rachunkowości, przepisów podatkowych
+			(podstawa prawna przetwarzania: art. 6 ust. 1 lit. c) RODO - przetwarzanie jest niezbędne do
+			wypełnienia obowiązku prawnego ciążącego na administratorze). W tym celu dane będą
+			przechowywane przez okres niezbędny do wykonania obowiązków prawnych ciążących na
+			administratorze, przy czym, jeżeli został określony w odpowiednich przepisach, przez taki
+			okres. Dane osobowe mogą być przekazywane organom publicznym na mocy obowiązujących przepisów
+			prawa. Dane osobowe mogą być ujawniane procesorom, czyli podmiotom przetwarzającym na zlecenie
+			administratora danych.</p> <p class="mt-4">Dane osobowe przetwarzane będą w oparciu o obowiązujące przepisy prawa. Informuje się o
+			przysługującym, w zakresie i z zastrzeżenie wyjątków przewidzianych przepisami prawa, prawie
+			dostępu do swoich danych osobowych, prawie do ich sprostowania, prawie do ich usunięcia,
+			prawie do ograniczenia przetwarzania, prawie do przenoszenia danych, a także o prawie do
+			wniesienia sprzeciwu. Informuje się o prawie wniesienia skargi do organu nadzorczego. Organem
+			nadzorczym jest Prezes Urzędu Ochrony Danych Osobowych z siedzibą przy ul. Stawki 2 w
+			Warszawie. Podanie danych osobowych jest dobrowolne. Jednakże podanie danych osobowych jest
+			warunkiem zawarcia i wykonywania Umowy i bez ich podania nie będzie ona mogła być zawarta ani
+			wykonywana.</p></div></section>`);function h(o){var i=s();t("tfrt3b",n=>{var r=c();z(()=>{w.title="EVIM - RODO"}),a(n,r)}),a(o,i)}export{h as component};
